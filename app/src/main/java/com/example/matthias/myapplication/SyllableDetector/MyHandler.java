ef7ff3b5cfeedb3d1c3f59bd9503dfa500c7736e
Matthias Niel <matthias.niel@student.tugraz.at>
2018-11-19 13:02:14 +0100

    add debug output

--- app/src/main/java/com/example/matthias/myapplication/SyllableDetector/MyHandler.java
+++ app/src/main/java/com/example/matthias/myapplication/SyllableDetector/MyHandler.java
@@ -2,10 +2,12 @@ package com.example.matthias.myapplication.SyllableDetector;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 
 import com.example.matthias.myapplication.AnalyzedFile;
@@ -21,6 +23,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MyHandler extends Handler {
     private static final String LOG_TAG = "MyHandler";
@@ -33,17 +36,20 @@ public class MyHandler extends Handler {
         mcontext = context;
         msyllableDetector = syllableDetector;
     }
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public void handleMessage(Message msg) {
         FolderToAnalyze f = (FolderToAnalyze) msg.obj;
         ArrayList<Uri> files = f.getfiles();
         int i = 0;
+        List<SyllableResult> results = new ArrayList<>();
         for(Uri file : files){
             try {
                 i++;
                 Log.d(LOG_TAG, "Analyzing file " + Integer.toString(i) + "/" + Integer.toString(files.size()));
                 InputStream is = mcontext.getContentResolver().openInputStream(file);
-                int numPeaks = msyllableDetector.getNumPeaks(is);
-                analyzedFiles.add(new AnalyzedFile(HelperFunctions.getFileID(file), numPeaks));
+                SyllableResult s = msyllableDetector.getNumPeaks(is, HelperFunctions.getFileID(file));
+                results.add(s);
+                analyzedFiles.add(new AnalyzedFile(HelperFunctions.getFileID(file), s.numSyllables));
                 is.close();
             } catch (IOException e) {
                 e.printStackTrace();
@@ -59,9 +65,14 @@ public class MyHandler extends Handler {
                 Log.d(LOG_TAG, "Directory not created");
             }
             try {
-                File file = new File(folder, f.getoutFileName());
+                File file = new File(folder, f.getoutFileName() + ".json");
                 FileOutputStream fOut = new FileOutputStream(file);
                 JsonDataWriter.write(fOut, analyzedFiles);
+                if(BuildConfig.DEBUG){
+                    File debugInfoFile = new File(folder, f.getoutFileName() +"_debug.json");
+                    FileOutputStream fOutDebug = new FileOutputStream(debugInfoFile);
+                    SyllableDebugJsonWriter.write(fOutDebug, results);
+                }
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
